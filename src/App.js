c93554359c24d4d884384e46e8b75e3a4ede9ffa
David Williams <d.williams1994@gmail.com>
2018-07-10 09:52:49 -0500

    Added error handling in case MQTT message is not valid JSON

--- src/App.js
+++ src/App.js
@@ -6,85 +6,92 @@ import MQTT from './MQTT';
 import {filter, map} from 'rxjs/operators';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: false,
-            time: {
-                value: 0,
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false,
+      time: {
+        value: 0,
+        updated: new Date().getTime()
+      },
+      shown: false,
+      fadeDuration: 2000
+    }
+  }
+
+  componentDidMount() {
+    MQTT.subscribe('Paradox/Houdini/Mirror/Clock/Commands')
+      .pipe(
+        filter(response => response),
+        map(payload => {
+          try {
+            return JSON.parse(payload);
+          }
+          catch (e) {
+            console.log(e);
+          }
+        }))
+      .subscribe(commandObject => {
+        try{
+          if (commandObject.time) {
+            let time = this.state.time;
+            try {
+              time = commandObject.time.split(':').map(t => Number(t));
+              time = time[0] * 60 + time[1];
+              if (isNaN(time)) time = this.state.time;
+            } catch (e) {
+            }
+            this.setState({
+              time: {
+                value: time,
                 updated: new Date().getTime()
-            },
-            shown: false,
-            fadeDuration: 2000
-        }
-    }
-
-    componentDidMount() {
-        MQTT.subscribe('Paradox/Houdini/Mirror/Clock/Commands')
-            .pipe(
-                filter(response => response),
-                map(payload => {
-                    try {
-                        return JSON.parse(payload);
-                    }
-                    catch (e) {
-                    }
-                }))
-            .subscribe(commandObject => {
-                if (commandObject.time) {
-                    let time = this.state.time;
-                    try {
-                        time = commandObject.time.split(':').map(t => Number(t));
-                        time = time[0] * 60 + time[1];
-                        if (isNaN(time)) time = this.state.time;
-                    } catch (e) {
-                    }
-                    this.setState({
-                        time: {
-                            value: time,
-                            updated: new Date().getTime()
-                        }
-                    });
-                }
-                else if (commandObject.hint) {
-                    this.setState({
-                        hint: commandObject.hint,
-                        duration: commandObject.duration,
-                    });
-                }
-                else if (commandObject.command) {
-                    switch (commandObject.command) {
-                        case 'start':
-                            this.setState({active: true});
-                            break;
-                        case 'pause':
-                            this.setState({active: false, fadeDuration: commandObject.duration || 2000});
-                            break;
-                        case 'fadeout':
-                            this.setState({shown: false, fadeDuration: commandObject.duration || 2000});
-                            break;
-                        case 'fadein':
-                            this.setState({shown: true, fadeDuration: commandObject.duration || 2000});
-                            break;
-                        default:
-                            console.warn(`Unexpected command object: ${JSON.stringify(commandObject)}`);
-                    }
-                }
+              }
             });
-    }
+          }
+          else if (commandObject.hint) {
+            this.setState({
+              hint: commandObject.hint,
+              duration: commandObject.duration,
+            });
+          }
+          else if (commandObject.command) {
+            switch (commandObject.command) {
+            case 'start':
+              this.setState({active: true});
+              break;
+            case 'pause':
+              this.setState({active: false, fadeDuration: commandObject.duration || 2000});
+              break;
+            case 'fadeout':
+              this.setState({shown: false, fadeDuration: commandObject.duration || 2000});
+              break;
+            case 'fadein':
+              this.setState({shown: true, fadeDuration: commandObject.duration || 2000});
+              break;
+            default:
+              console.warn(`Unexpected command object: ${JSON.stringify(commandObject)}`);
+            }
+          }
+        }
+        catch(e){
+          console.log(e);
+        }
+      });
+  }
 
-    render() {
-        console.debug('App render');
-        const appClasses = `App ${this.state.shown ? 'shown' : 'hidden'}`;
-        const appStyle = {animationDuration: `${this.state.fadeDuration}ms`};
-        return (
-            <div className={appClasses}
-                 style={appStyle}>
-                <Clock active={this.state.active} time={this.state.time}></Clock>
-                <Hint text={this.state.hint} duration={this.state.duration}></Hint>
-            </div>
-        );
-    }
+
+render() {
+  console.debug('App render');
+  const appClasses = `App ${this.state.shown ? 'shown' : 'hidden'}`;
+  const appStyle = {animationDuration: `${this.state.fadeDuration}ms`};
+  return (
+      <div className={appClasses}
+    style={appStyle}>
+      <Clock active={this.state.active} time={this.state.time}></Clock>
+      <Hint text={this.state.hint} duration={this.state.duration}></Hint>
+      </div>
+  );
+}
 }
 
 export default App;
