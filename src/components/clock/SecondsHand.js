7ba9e6214239291b808fafd7898d763a44fd7e3e
David Williams <d.williams1994@gmail.com>
2018-07-12 14:06:19 -0500

    fixed jump

--- src/components/clock/SecondsHand.js
+++ src/components/clock/SecondsHand.js
@@ -5,12 +5,16 @@ export default class SecondsHand extends PureComponent {
     animationClassIdx = 0;
 
     render() {
-        let rotate = (this.props.time % 60) * 360 / 60;
+        let rotate = (Math.PI * 3/2) + Math.atan2(Math.cos((-Math.PI * this.props.time)/30), ((3/5)*Math.sin((-Math.PI * this.props.time)/30)));
+      if(rotate === (Math.PI * 2)){
+       // console.log("greater than");
+        rotate = 0;
+}
         this.animationClassIdx = !this.animationClassIdx & 1;
         return (
             <div className={"ss " + (this.props.animated ? ("tick-animation-" + this.animationClassIdx) : "")}>
                 <div className="s"
-                     style={{transform: `translate(0px, calc(85vh / 3 / 2)) rotate(${rotate}deg) translate(0px, calc(-85vh / 3 / 2))`, transition: 'all 1s'}}></div>
+                     style={{transform: `translate(0px, calc(85vh / 3 / 2)) rotate(${rotate}rad) translate(0px, calc(-85vh / 3 / 2))`, transition: 'all 1s'}}></div>
             </div>
         );
     }
